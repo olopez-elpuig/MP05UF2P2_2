fb68e0c594d74e1636e20fc5f4aa81870a416cfe
olopez <olopez@elpuig.xeill.net>
2021-05-27 17:48:31 +0200

    prueb

--- src/test/java/ex2/HashTableTest.java
+++ src/test/java/ex2/HashTableTest.java
@@ -214,32 +214,88 @@ class HashTableTest {
                 " bucket[1] = [1, av2] -> [01, c] -> [12, d]\n" +
                 " bucket[2] = [2, bv2]", ht.toString());
     }
-    @org.junit.jupiter.api.Test
-    void hastable_get_comprovar_retorn() {
-        HashTable ht = new HashTable();
-        for(int i=0; i<30; i++) {
-            final String key = String.valueOf(i);
-            ht.put(key, key);
-        }
 
-        Assertions.assertEquals("0", ht.get("0"));
-        Assertions.assertEquals("11", ht.get("11"));
-        ht.drop("11");
-        Assertions.assertNull(ht.get("11"));
-        Assertions.assertEquals("9", ht.get("9"));
-        Assertions.assertEquals("21", ht.get("21"));
-        Assertions.assertEquals("\n" +
-                " bucket[0] = [0, 0] -> [22, 22]\n" +
-                " bucket[1] = [1, 1] -> [12, 12] -> [23, 23]\n" +
-                " bucket[2] = [2, 2] -> [13, 13] -> [24, 24]\n" +
-                " bucket[3] = [3, 3] -> [14, 14] -> [25, 25]\n" +
-                " bucket[4] = [4, 4] -> [15, 15] -> [26, 26]\n" +
-                " bucket[5] = [5, 5] -> [16, 16] -> [27, 27]\n" +
-                " bucket[6] = [6, 6] -> [17, 17] -> [28, 28]\n" +
-                " bucket[7] = [7, 7] -> [18, 18] -> [29, 29]\n" +
-                " bucket[8] = [8, 8] -> [19, 19]\n" +
-                " bucket[9] = [9, 9]\n" +
-                " bucket[14] = [20, 20]\n" +
-                " bucket[15] = [10, 10] -> [21, 21]", ht.toString());
+    @org.junit.jupiter.api.Test
+    void hastable_get_comprovar_buida() {
+        HashTable ht = new HashTable();
+
+
+
+        Assertions.assertNull( ht.get("1"));
+    }
+
+    @org.junit.jupiter.api.Test
+    void hastable_get_comprovar() {
+        HashTable ht = new HashTable();
+        ht.put("1", "a");
+
+
+
+        Assertions.assertEquals( "a" ,ht.get("1"));
+    }
+
+    @org.junit.jupiter.api.Test
+    void hastable_get_comprovar_posicio_buida() {
+        HashTable ht = new HashTable();
+        ht.put("1", "a");
+        ht.put("2","b");
+
+
+
+        Assertions.assertEquals( "b" ,ht.get("2"));
+    }
+
+    @org.junit.jupiter.api.Test
+    void hastable_get_comprovar_colissio_1() {
+        HashTable ht = new HashTable();
+        ht.put("1", "a");
+        ht.put("2","b");
+
+        //Element colissio
+        ht.put("01", "c");
+        ht.put("12", "d");
+
+
+        Assertions.assertEquals( "a" ,ht.get("1"));
+    }
+
+    @org.junit.jupiter.api.Test
+    void hastable_get_comprovar_colissio_2() {
+        HashTable ht = new HashTable();
+        ht.put("1", "a");
+        ht.put("2","b");
+
+        //Element colissio
+        ht.put("01", "c");
+        ht.put("12", "d");
+
+        Assertions.assertEquals( "c" ,ht.get("01"));
+    }
+
+    @org.junit.jupiter.api.Test
+    void hastable_get_comprovar_colissio_3() {
+        HashTable ht = new HashTable();
+        ht.put("1", "a");
+        ht.put("2","b");
+
+        //Element colissio
+        ht.put("01", "c");
+        ht.put("12", "d");
+
+        Assertions.assertEquals( "d" ,ht.get("12"));
+    }
+    @org.junit.jupiter.api.Test
+    void hastable_get_comprovar_colissio_3elemnts_noexist() {
+        HashTable ht = new HashTable();
+        ht.put("1", "a");
+        ht.put("2","b");
+
+        //Element colissio
+        ht.put("01", "c");
+        ht.put("12", "d");
+        //Este elemento no existe pero esta colisionado por los tres elementos del bucket [1]
+        ht.put("23", "e");
+
+        Assertions.assertEquals( "e" ,ht.get("23"));
     }
 }
